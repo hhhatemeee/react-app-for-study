145a9f00767596ceb6521225d138a0f33036061a
al0k1r <biryulin.sanya@icloud.com>
2022-01-08 14:43:26 +0300

    Refactoring code, use Formik component

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { useFormik } from 'formik'
+import { Form, Formik, Field, ErrorMessage } from 'formik'
 import s from './Login.module.css'
 import * as yup from 'yup'
 
@@ -8,38 +8,37 @@ const Login = (props) => {
         email: yup.string().typeError('Должна быть строкой').required('Обязательное поле'),
         password: yup.string().typeError('Должна быть строкой').required('Обязательное поле')
     })
+    const initialValues = {
+        email: '',
+        password: '',
+        remember: false,
+    }
+    const onSubmit = (values) => {
+        console.log(values)
+        props.login(values)
+    }
 
-    const formik = useFormik({
-        initialValues: {
-            email: '',
-            password: '',
-            remember: false,
-        },
-        validationSchema: validationForm,
-        onSubmit: values => {
-            props.login(values)
-        },
-    })
-    useEffect(() => {
 
-    }, [formik.values])
     return (
         <div>
             <h1>email</h1>
-            <div >
-                <form action="" onSubmit={formik.handleSubmit} style={{ display: 'flex', flexDirection: "column", maxWidth: '300px', margin: '5px' }}>
+            <Formik
+                initialValues={initialValues}
+                validationSchema={validationForm}
+                onSubmit={onSubmit}>
+                <Form action="" style={{ display: 'flex', flexDirection: "column", maxWidth: '300px', margin: '5px' }}>
                     <label htmlFor="email">Логин</label>
-                    <input type="text" id='email' name='email' {...formik.getFieldProps('email')} />
-                    {formik.touched.email && formik.errors.email ? <span className={s.errorText}>{formik.errors.email}</span> : null}
+                    <Field type="text" id='email' name='email' />
+                    <ErrorMessage style={{ color: 'red' }} name='email' />
                     <label htmlFor="password">Пароль</label>
-                    <input type="password" id='password' name='password' {...formik.getFieldProps('email')} />
-                    {formik.touched.password && formik.errors.password ? <span className={s.errorText}> {formik.errors.password}</span> : null}
+                    <Field type="password" id='password' name='password' />
+                    <ErrorMessage className={s.errorText} name='password' />
                     <div style={{ display: 'flex', alignItems: 'center' }}>
-                        <input type="checkbox" id='remember' name='remember' onChange={formik.handleChange} value={formik.values.remember} />remember me
+                        <Field type="checkbox" id='remember' name='remember' />remember me
                     </div>
                     <button type='submit'>Login</button>
-                </form>
-            </div>
+                </Form>
+            </Formik>
 
         </div >
     )
